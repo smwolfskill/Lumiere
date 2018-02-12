b8bb6c30d2d6eccc5a9c6dba2afca4aa91acde30
pmillar2 <pmillar2@illinois.edu>
2018-02-12 17:04:05 -0600

    Upload New File

---
+++ Lumiere/Assets/Scripts/WallTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A tile object that represents walls (Things the player cannot navigate through)
+/// </summary>
+public class WallTile : GameTile
+{
+    /// <summary>
+    /// Constructor for a wall tile. Walls are impassable.
+    /// </summary>
+    /// <param name="spritedata">The wall's sprite data.</param>
+    public WallTile(Sprite spritedata) : base(spritedata)
+    {
+        // Nothing to see here, move along.
+    }
+}
