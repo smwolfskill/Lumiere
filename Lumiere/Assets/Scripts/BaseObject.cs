3e4bb9dcef5728276c75f6a433ac50571336fff0
pmillar2 <pmillar2@illinois.edu>
2018-02-12 17:02:31 -0600

    Replace BaseObject.cs

--- Lumiere/Assets/Scripts/BaseObject.cs
+++ Lumiere/Assets/Scripts/BaseObject.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class BaseObject : ScriptableObject {
-	protected Sprite sprite;
+/// <summary>
+/// Base object class, designed for general use for all sub-classes that represent some object in the game.
+/// </summary>
+public abstract class BaseObject : ScriptableObject 
+{
+    protected Sprite sprite;    // Sprite data.
+    
+    /// <summary>
+    /// Getter function for sprite data.
+    /// </summary>
+    /// <returns>The sprite data of the object.</returns>
+    public Sprite getSprite() 
+    {
+        return sprite;
+    }
 
-	public Sprite getSprite() {
-		return sprite;
-	}
-
-	public void setSprite(Sprite sprite) {
-		this.sprite = sprite;
-	}
+    /// <summary>
+    /// Setter function for sprite data.
+    /// </summary>
+    /// <param name="sprite">The new sprite being set.</param>
+    public void setSprite(Sprite sprite) 
+    {
+        this.sprite = sprite;
+    }
 }
