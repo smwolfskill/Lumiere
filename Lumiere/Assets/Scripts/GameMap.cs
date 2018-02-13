d6782608afcc0cf9a3b1f8a2515b8554d7f76307
WyattHRichter <WyattHRichter@gmail.com>
2018-02-12 23:26:23 -0600

    added helper functions to GameMap, cleaned up some code. nothing much yet, just a first test commit

--- Lumiere/Assets/Scripts/GameMap.cs
+++ Lumiere/Assets/Scripts/GameMap.cs
@@ -7,11 +7,12 @@ using UnityEngine;
 /// </summary>
 public class GameMap
 {
-    private const double tileOffset = 32.0;    // Tile size offset in the game's units. A unit is defined by the position logic for objects not locked on the grid.
+    public const double tileOffset = 32.0;    // Tile size offset in the game's units. A unit is defined by the position logic for objects not locked on the grid.
+    public int width;
+    public int height;
+
     private GameTile[,] tileMap;
-    private int width;
-    private int height;
-    
+
     /// <summary>
     /// Constructor for the gamemap that includes width and height.
     /// </summary>
@@ -19,20 +20,61 @@ public class GameMap
     /// <param name="height">Height of the map in tiles.</param>
     public GameMap(int width, int height)
     {
-        buildMap(width, height);
+        this.width = width;
+        this.height = height;
+
+        buildMap();
     }
 
     /// <summary>
     /// Creates the actual map data, filling tile slots with required information.
     /// </summary>
-    /// <param name="width">Width of the map in tiles.</param>
-    /// <param name="height">Height of the map in tiles.</param>
-    private void buildMap(int width, int height)
+    private void buildMap()
     {
-        this.width = width;
-        this.height = height;
         this.tileMap = new GameTile[height, width];
-        // TODO: Call map generator function to establish array.
+
+        // Call one of many different map creation algorithms
+        buildMapSimpleMap();
+    }
+
+    /// <summary>
+    /// A map creation algorithm.
+    /// </summary>
+    private void buildMapSimpleMap()
+    {
+        
+    }
+
+    /// <summary>
+    /// Applies a GameTile to a coordinate in the tileMap.
+    /// </summary>
+    /// <returns>
+    /// True if the tile was placed correctly, false otherwise (such as when the tile
+    /// cannot be placed in the specified coordinate due to the coordinate being invalid)
+    /// </returns>
+    private bool setTile(int x, int y, GameTile gameTile)
+    {
+        if (!validTileSpace(x, y))
+            return false;
+
+        this.tileMap[y, x] = gameTile;
+
+        return true;
+    }
+
+    /// <summary>
+    /// Sets an area of the map (from left,top to left+width,top+height) all to a 
+    /// specified gameTile.
+    /// </summary>
+    private void setTileArea(int left, int top, int width, int height, GameTile gameTile)
+    {
+        for (int x = left; x < left + width; x++)
+        {
+            for (int y = top; y < top + height; y++)
+            {
+                setTile(x, y, gameTile);
+            }
+        }
     }
 
     /// <summary>
@@ -70,10 +112,18 @@ public class GameMap
         int realY = (int)Math.Floor(y / tileOffset);
 
         // Dummy check 2.
-        if (realX > this.width || realY > this.height)
+        if (!validTileSpace(realX, realY))
             return null;
         
         // Return tile data.
         return this.tileMap[realY, realX];
+    }
+
+    /// <summary>
+    /// Checks if a coordinate is one that exists within the boundaries of the map/
+    /// </summary>
+    private bool validTileSpace(int x, int y)
+    {
+        return (x >= 0 && y >= 0 && x < width && y < height);
     }
 }
