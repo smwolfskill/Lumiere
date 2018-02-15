d3ad2e69e28136739cb14ab069d22df9229c977c
rizaneeves <rizaneeves>
2018-02-14 20:28:55 -0600

    Remove EntityAction - unneeded abstraction (for now)

--- Lumiere/Assets/Scripts/Entity.cs
+++ Lumiere/Assets/Scripts/Entity.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Entity : BaseObject {
-	protected EntityAction[] actions; 	//will hold all actions that this entity can perform
+	protected Action[] actions; 	//will hold all actions that this entity can perform
 }
