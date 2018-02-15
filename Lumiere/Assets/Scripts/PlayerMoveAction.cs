ef138ff2e9ab1cd00cef867d2fdb6dcfe6508633
rizaneeves <rizaneeves>
2018-02-14 20:31:21 -0600

    Add a movement action for player based on input

---
+++ Lumiere/Assets/Scripts/PlayerMoveAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Lumiere/Actions/PlayerMoveAction")]
+public class PlayerMoveAction : Action 
+{
+	public float speed = 1f;
+
+	public override bool validate (GameObject obj)
+	{
+		return true;
+	}
+
+	public override bool execute (GameObject obj)
+	{
+		Rigidbody2D rigidbody = obj.GetComponent<Rigidbody2D> ();
+		if (rigidbody == null) 
+		{
+			return false;
+		}
+
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		if (Mathf.Abs(h) >= Mathf.Abs(v)) 
+		{
+			rigidbody.velocity = new Vector2 (h, 0f);
+		} 
+		else 
+		{
+			rigidbody.velocity = new Vector2 (0f, v);	
+		}
+
+		return true;
+	}
+}
