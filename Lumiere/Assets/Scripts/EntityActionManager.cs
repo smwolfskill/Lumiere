cca9b4e62f241491634aeb7d0164005164025ec3
rizaneeves <rizaneeves>
2018-02-14 20:32:50 -0600

    Add placeholder EntityActionManager

---
+++ Lumiere/Assets/Scripts/EntityActionManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityActionManager : MonoBehaviour {
+
+	//public Entity entity;
+	public Action action;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (this.gameObject == null) 
+		{
+			return;
+		}
+
+		if (action.validate (this.gameObject)) 
+		{
+			
+			action.execute (this.gameObject);
+		}
+	}
+}
