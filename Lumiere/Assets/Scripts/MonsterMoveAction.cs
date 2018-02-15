4eb5ebdfcb85e77b977672a4fcf3a58b5c43c29d
rizaneeves <rizaneeves>
2018-02-14 21:39:00 -0600

    Move assets and add Monster movement

---
+++ Lumiere/Assets/Scripts/MonsterMoveAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonsterMoveAction : Action 
+{
+	public float speed;
+
+	public override bool validate (GameObject obj)
+	{
+		return true;
+	}
+}
---
+++ Lumiere/Assets/Scripts/RandomMoveAction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Lumiere/Actions/RandomMoveAction")]
+public class RandomMoveAction : MonsterMoveAction 
+{
+	public float directionChangeTimer = 5f;
+	float timer = 0f;
+	bool initialized = false;
+
+	protected void Initialize()
+	{
+		initialized = true;
+	}
+
+	public override bool validate (GameObject obj)
+	{
+		if (!initialized) 
+		{
+			Initialize ();
+			return true;
+		}
+		
+		if (timer >= directionChangeTimer) 
+		{
+			timer = Time.deltaTime;
+			return true;
+		}
+
+		timer += Time.deltaTime;
+		return false;
+	}
+	public override bool execute (GameObject obj)
+	{
+		Rigidbody2D rigidbody = obj.GetComponent<Rigidbody2D> ();
+		if (rigidbody == null) 
+		{
+			return false;
+		}
+
+		float isHorizontal = Random.Range (-1.0f, 1.0f);
+		float directionModifier = Mathf.Sign(Random.Range (-1.0f, 1.0f));
+		if (isHorizontal >= 0.0f) 
+		{
+			rigidbody.velocity = new Vector2 (directionModifier * speed, 0f);
+		} 
+		else 
+		{
+			rigidbody.velocity = new Vector2 (0f, directionModifier * speed);	
+		}
+
+		return true;
+	}
+}
