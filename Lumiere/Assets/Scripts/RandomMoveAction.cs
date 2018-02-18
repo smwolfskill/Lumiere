62a9fa8d8e4a102996519bdac4abb6fe31d8e0ed
Sheel Parekh <sdparek2@illinois.edu>
2018-02-18 12:34:33 -0500

    Another attempt to fix spacing

--- Lumiere/Assets/Scripts/RandomMoveAction.cs
+++ Lumiere/Assets/Scripts/RandomMoveAction.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Lumiere/Actions/EntityActions/RandomMoveAction")]
 public class RandomMoveAction : MonsterMoveAction 
 {
-	public float directionChangeTimer = 5f;
-	private float timer = 0f;
-	private bool initialized = false;
+    public float directionChangeTimer = 5f;
+    private float timer = 0f;
+    private bool initialized = false;
 
-	public override bool Validate(GameObject obj)
+    public override bool Validate(GameObject obj)
     {
         if (!base.Validate(obj)) { return false; }
 
@@ -20,24 +20,24 @@ public class RandomMoveAction : MonsterMoveAction
             //We have validated once, set the timer to what it should be after one validation
             timer = Time.deltaTime;
             return true;
-		}
-		
+        }
+
         //Otherwise, only change movement every directionChangeTimer seconds.
-		if (timer >= directionChangeTimer) 
-		{
+        if (timer >= directionChangeTimer)
+        {
             //Reset the timer to what it would've been after one validation
-			timer = Time.deltaTime;
-			return true;
-		}
+            timer = Time.deltaTime;
+            return true;
+        }
 
         //Increment the timer every time Action is validated
-		timer += Time.deltaTime;
-		return false;
-	}
+        timer += Time.deltaTime;
+        return false;
+    }
 
-	public override bool Execute(GameObject obj)
-	{
-		Rigidbody2D rigidbody = obj.GetComponent<Rigidbody2D> ();
+    public override bool Execute(GameObject obj)
+    {
+        Rigidbody2D rigidbody = obj.GetComponent<Rigidbody2D> ();
 
         //Safety check for rigidbody
         if (rigidbody == null)
@@ -46,18 +46,18 @@ public class RandomMoveAction : MonsterMoveAction
         }
 
         //Choose a random axis to move along (either the horizontal or the vertical)
-		bool isHorizontal = Random.Range (-1.0f, 1.0f) >= 0.0f;
+        bool isHorizontal = Random.Range (-1.0f, 1.0f) >= 0.0f;
         //Choose whether to move in the positive or negative sense
-		float directionModifier = Mathf.Sign(Random.Range (-1.0f, 1.0f));
-		if (isHorizontal) 
-		{
-			rigidbody.velocity = new Vector2 (directionModifier * speed, 0f);
-		} 
-		else 
-		{
-			rigidbody.velocity = new Vector2 (0f, directionModifier * speed);	
-		}
+        float directionModifier = Mathf.Sign(Random.Range (-1.0f, 1.0f));
+        if (isHorizontal)
+        {
+            rigidbody.velocity = new Vector2 (directionModifier * speed, 0f);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2 (0f, directionModifier * speed);
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
