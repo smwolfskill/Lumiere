9610f24a65ce659c9ec4d35fa4c7100ce609f2f1
pmillar2 <pmillar2@illinois.edu>
2018-02-21 22:19:08 -0600

    Misc Item Type Added.

---
+++ Lumiere/Assets/Scripts/Items/PrimaryTypes/MiscItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Representation of an item that can neither be equipped or used somehow (crafting, collectables, money, etc)
+/// </summary>
+public class MiscItem : GameItem
+{
+    // TODO: Anything here that is needed?
+}
