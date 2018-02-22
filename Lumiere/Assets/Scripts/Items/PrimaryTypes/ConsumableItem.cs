1c09d9b3b47d277cf5cfde08bb247b48503f3d51
pmillar2 <pmillar2@illinois.edu>
2018-02-21 22:19:41 -0600

    Consumable Item Type Added.

---
+++ Lumiere/Assets/Scripts/Items/PrimaryTypes/ConsumableItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Representation of an item that can be consumed.
+/// </summary>
+public class ConsumableItem
+{
+    // TODO: Implement consumable functionality, likely in IT4.
+    // Note for this to be fully functional Player and Monster need to be abstracted from an object that has generic stats (such as HP)
+
+    /// <summary>
+    /// Function called when a player consumes the item.
+    /// </summary>
+    /// <param name="player">The player.</param>
+    public void Consume(Player player)
+    {
+        // TODO: Implement functionality.
+    }
+}
