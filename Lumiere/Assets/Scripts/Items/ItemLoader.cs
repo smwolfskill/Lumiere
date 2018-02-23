abb25288e281c60846360672006cdd5591332d95
pmillar2 <pmillar2@illinois.edu>
2018-02-22 21:59:51 -0600

    Item Loader skeleton added.

---
+++ Lumiere/Assets/Scripts/Items/ItemLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// A static class that loads specified item data from a file and returns that data in the form of an item.
+/// </summary>
+public static class ItemLoader
+{
+    /// <summary>
+    /// Loads an item by filename into the form of a GameItem object.
+    /// </summary>
+    /// <param name="filename">Location where information is stored.</param>
+    /// <returns>A functional GameItem with the data from the file.</returns>
+    public static GameItem LoadItem(string filename)
+    {
+        return null;
+    }
+}
