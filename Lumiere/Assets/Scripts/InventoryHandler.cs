37e7a9b34bbb4569eb6e851c095755813ed74d39
pmillar2 <pmillar2@illinois.edu>
2018-02-22 22:00:06 -0600

    InventoryHandler skeleton file uploaded.

---
+++ Lumiere/Assets/Scripts/InventoryHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// A class designed to handle the GUI interface for the player to utilize their inventory.
+/// </summary>
+public class InventoryHandler
+{
+    private Inventory inventory;
+
+    // TODO: Implement the following
+    // * Interface with main unity engine for the inventory class.
+    // * Visuals to handle displaying the grid for the user.
+    // * Interface should be interactive, be able to use the Inventory classes functions for moving items.
+}
