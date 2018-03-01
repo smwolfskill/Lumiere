9da2aa60a5f796f749f5d59683802b291acf53b6
Craig Swearingen <cswear@illinoisrocstar.com>
2018-02-28 19:49:16 -0600

    Added Test for Secret and Hideout rooms

---
+++ Lumiere/Assets/Scripts/Tests/HideoutRoomGenerationTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+/// <summary>
+/// Basic testing class for hideout room generation.
+/// </summary>
+public class HideoutRoomTest 
+{
+    HideoutRoomType hideoutRoomType;
+    HideoutRoomObj hideoutRoomObj;
+    Map map;
+    GameObject mapGameObject;
+
+    [SetUp]
+    public void Init()
+    {
+        mapGameObject = new GameObject ("Map");
+        Map map = new Map (50, 50, 1, mapGameObject);
+        hideoutRoomObj = new HideoutRoomObj (map);
+        hideoutRoomType = Resources.Load<HideoutRoomType> ("Rooms/Hideout");
+        Assert.IsNotNull (map);
+        Assert.IsNotNull (hideoutRoomObj);
+        Assert.IsNotNull (hideoutRoomType);
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        if (hideoutRoomType != null) 
+        {
+            Resources.UnloadAsset (hideoutRoomType);
+        }
+
+        if (mapGameObject != null) 
+        {
+            GameObject.Destroy (mapGameObject);
+        }
+
+        map = null;
+        hideoutRoomObj = null;
+    }
+
+    /// <summary>
+    /// Tests whether the room tiles spawned.
+    /// </summary>
+    [Test]
+    public void TestTilesSpawned()
+    {
+        hideoutRoomObj.GenRoom ();
+        Assert.IsTrue (hideoutRoomObj.tileObjs.Count > 0);
+    }
+
+    /// <summary>
+    /// Tests whether the room exists in the scene.
+    /// </summary>
+    [Test]
+    public void TestRoomInGame()
+    {
+        GameObject room = GameObject.Find ("HideoutRoomObj");
+        Assert.IsNotNull (room);
+    }
+
+}
---
+++ Lumiere/Assets/Scripts/Tests/SecreetRoomGenerationTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+/// <summary>
+/// Basic testing class for secret room generation.
+/// </summary>
+public class SecretRoomTest 
+{
+    SecretRoomType secretRoomType;
+    SecretRoomObj secretRoomObj;
+    Map map;
+    GameObject mapGameObject;
+
+    [SetUp]
+    public void Init()
+    {
+        mapGameObject = new GameObject ("Map");
+        Map map = new Map (50, 50, 1, mapGameObject);
+        secretRoomObj = new SecretRoomObj (map);
+        secretRoomType = Resources.Load<SecretRoomType> ("Rooms/Secret");
+        Assert.IsNotNull (map);
+        Assert.IsNotNull (secretRoomObj);
+        Assert.IsNotNull (secretRoomType);
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        if (secretRoomType != null) 
+        {
+            Resources.UnloadAsset (secretRoomType);
+        }
+
+        if (mapGameObject != null) 
+        {
+            GameObject.Destroy (mapGameObject);
+        }
+
+        map = null;
+        secretRoomObj = null;
+    }
+
+    /// <summary>
+    /// Tests whether the room tiles spawned.
+    /// </summary>
+    [Test]
+    public void TestTilesSpawned()
+    {
+        secretRoomObj.GenRoom ();
+        Assert.IsTrue (secretRoomObj.tileObjs.Count > 0);
+    }
+
+    /// <summary>
+    /// Tests whether the room exists in the scene.
+    /// </summary>
+    [Test]
+    public void TestRoomInGame()
+    {
+        GameObject room = GameObject.Find ("SecretRoomObj");
+        Assert.IsNotNull (room);
+    }
+
+}
