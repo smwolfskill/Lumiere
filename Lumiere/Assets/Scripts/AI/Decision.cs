af8e5e970547c8dd5aa106bd319f06badf8d2a99
rizaneeves <rizaneeves>
2018-03-06 17:09:16 -0600

    Add template for state machine paradigm

---
+++ Lumiere/Assets/Scripts/AI/Decision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A class to represent decisions that an AI can make. 
+/// The decision made will impact the next state the AI will transition to.
+/// </summary>
+public abstract class Decision : ScriptableObject 
+{
+    public abstract bool Decide ();
+}
---
+++ Lumiere/Assets/Scripts/AI/State.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A simple class to represent a State in the overall state machine.
+/// A state contains several actions that should be performed when the AI is in the state.
+/// Furthermore, the state also contains its possible transitions.
+/// We can easily create states as ScriptableObjects and store them for re-use.
+/// </summary>
+[CreateAssetMenu(menuName = "Lumiere/AI/State")]
+public class State : ScriptableObject
+{
+    public Action[] actions;
+    public Transition[] transitions;
+
+    /// <summary>
+    /// Updates the state of the state controller based on the transitions given, and executes actions.
+    /// </summary>
+    /// <param name="stateController">State controller.</param>
+    public void UpdateState(StateController stateController)
+    {
+        DoActions (stateController);
+        CheckTransitions (stateController);
+    }
+
+    /// <summary>
+    /// Executes the actions to be performed in this state in the order they were specified.
+    /// </summary>
+    /// <param name="stateController">The state controller of the AI.</param>
+    private void DoActions (StateController stateController)
+    {
+        //TODO: implement
+    }
+
+    /// <summary>
+    /// Check all the transitions and evaluate what the next state should be.
+    /// Update the controller's state when necessary. Checks the transitions.
+    /// </summary>
+    /// <param name="stateController">The state controller of the AI.</param>
+    private void CheckTransitions(StateController stateController)
+    {
+        //TODO: implement
+    }
+
+
+}
