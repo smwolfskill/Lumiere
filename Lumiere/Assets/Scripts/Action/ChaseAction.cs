0d32d63c7f3fb2199114c7346f11d01d40c60041
rizaneeves <rizaneeves>
2018-03-08 21:55:04 -0600

    Add template for chasing action

---
+++ Lumiere/Assets/Scripts/Action/ChaseAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseAction : MonsterMoveAction 
+{
+    /// <summary>
+    /// The distance to stop chasing the target.
+    /// </summary>
+    public float stoppingDistance;
+
+    /// <summary>
+    /// Checks whether this action should be executed or not for the specified GameObject.
+    /// </summary>
+    /// <param name="obj">The GameObject that wants to execute this action.</param>
+    /// <returns>Returns true if this action should be executed, false otherwise.</returns>
+    public override bool Validate (GameObject obj)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    /// <summary>
+    /// Executes this action for the specified GameObject.
+    /// </summary>
+    /// <param name="obj">The GameObject that wants to execute this action.</param>
+    /// <returns>Returns true if this action is executed successfully, false otherwise.</returns>
+    public override bool Execute (GameObject obj)
+    {
+        throw new System.NotImplementedException ();
+    }
+}
