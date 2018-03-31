c18a72b7cd6391aa524f9cf9d0a2d2ce0350e3ae
pmillar2 <pmillar2@illinois.edu>
2018-03-30 20:10:46 -0500

    Updated Equippable Item.

--- Lumiere/Assets/Scripts/Items/PrimaryTypes/EquippableItem.cs
+++ Lumiere/Assets/Scripts/Items/PrimaryTypes/EquippableItem.cs
@@ -5,19 +5,49 @@
 /// </summary>
 public class EquippableItem : GameItem
 {
-    // Stats will be added here at some point to store additional information about these sorts of items and how they affect players.
+    // Slot the item is equipped into.
+    private EquipmentManager.EquipSlot slot;
 
     /// <summary>
-    /// Enumeration that keeps track of slots items can be put into.
+    /// Base Equippable Item constructor.
     /// </summary>
-    public enum EquipSlot
+    public EquippableItem() : base()
     {
-        HEAD,
-        WEAPON,
-        LEGS,
-        CHEST,
-        GLOVES,
-        RING,
-        NECK
+        this.slot = EquipmentManager.EquipSlot.CHEST;
+    }
+
+    /// <summary>
+    /// Constructor for an EquippableItem.
+    /// Overrides the item quantity and max stack count to 1 since equippables cannot stack.
+    /// </summary>
+    /// <param name="gui">Sprite representing the item in the inventory interface.</param>
+    /// <param name="ground">Sprite representing the item on the ground.</param>
+    /// <param name="newName">Name of the item.</param>
+    /// <param name="newDesc">Description of the item.</param>
+    /// <param name="val">Value of the item in whatever unit.</param>
+    /// <param name="rareness">Rarity of the item for rareness systems.</param>
+    /// <param name="newMaxStack">Maximum times the item can stack in an inventory slot.</param>
+    /// <param name="itemQuantity">Amount of items in this stack.</param>
+    /// <param name="itemID">The ID of the item.</param>
+    /// <param name="newSlot">Slot the equippable can be fit into. Defaults to chest.</param>
+    public EquippableItem(Sprite gui, Sprite ground, string newName, string newDesc, double val, ItemRarity rareness, int itemQuantity = 1, int newMaxStack = 1, int itemID = -1, EquipmentManager.EquipSlot newSlot = EquipmentManager.EquipSlot.CHEST) : base(gui, ground, newName, newDesc, val, rareness, 1, 1, itemID)
+    {
+        this.slot = newSlot;
+    }
+
+    /// <summary>
+    /// Getters and Setters for the slot.
+    /// </summary>
+    public EquipmentManager.EquipSlot Slot
+    {
+        get
+        {
+            return slot;
+        }
+
+        set
+        {
+            slot = value;
+        }
     }
 }
