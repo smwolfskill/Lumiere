8500745861ba3c1b8c52e9138f39a05094c3f4c6
pmillar2 <pmillar2@illinois.edu>
2018-03-30 20:08:14 -0500

    Equipment Manager Class Added

---
+++ Lumiere/Assets/Scripts/Items/EquipmentManager.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager
+{
+    /// <summary>
+    /// Enumeration that keeps track of slots items can be put into.
+    /// Each item can correspond to a specific slot.
+    /// </summary>
+    public enum EquipSlot
+    {
+        HEAD,
+        LEGS,
+        CHEST,
+        GLOVES,
+        RING,
+        NECK
+    }
+
+    // Holds onto the current equipment in each possible slot (EquipSlots, 1 per) and the usable items in the hotbar.
+    private EquippableItem[] equipment;
+    private UsableItem[] hotbar;
+
+    /// <summary>
+    /// Constructor for equipment manager.
+    /// </summary>
+    /// <param name="hotBarSize">Size of the player's hotbar, this is how many weapons usable items can be quick-swapped without opening a manager menu.</param>
+    public EquipmentManager(int hotBarSize = 10)
+    {
+        this.equipment = new EquippableItem[Enum.GetNames(typeof(EquipSlot)).Length];
+        this.hotbar = new UsableItem[hotBarSize];
+    }
+
+    #region Equipping And Unequipping
+    /// <summary>
+    /// Attempts to equip a given item to the equipment manager.
+    /// </summary>
+    /// <param name="item">The equippable item attempting to be added.</param>
+    /// <returns>True if successful, False otherwise.</returns>
+    public bool Equip(EquippableItem item)
+    {
+        // TODO: Implement.
+        return false;
+    }
+
+    /// <summary>
+    /// Attempts to dequip a given item from the equipment manager.
+    /// </summary>
+    /// <param name="slot">The slot type for the item being removed.</param>
+    /// <returns>The item if it was removed, null if there was no item or removal somehow failed.</returns>
+    public EquippableItem DeEquip(EquipSlot slot)
+    {
+        // TODO: Implement.
+        return null;
+    }
+
+    /// <summary>
+    /// Fetches the item data associated with a particular item slot.
+    /// </summary>
+    /// <param name="slot">The slot of the equipped item.</param>
+    /// <returns>The item if the slot contained an item. Null otherwise.</returns>
+    public EquippableItem GetEquippedItem(EquipSlot slot)
+    {
+        // TODO: Implement.
+        return null;
+    }
+
+    /// <summary>
+    /// Attempts to get the usable item at a given hotbar index.
+    /// </summary>
+    /// <param name="index">Index on the hotbar the item is located.</param>
+    /// <returns>The usable item if it exists, null if the hotbar slot is empty or out of bounds.</returns>
+    public UsableItem GetHotbarItem(int index)
+    {
+        // TODO: Implement.
+        return null;
+    }
+
+    /// <summary>
+    /// Attempts to add an item onto the hotbar.
+    /// </summary>
+    /// <param name="item">The item to be added.</param>
+    /// <param name="index">The hotbar index to place the item.</param>
+    /// <returns>True if successful, False if the slot already contains an item or the function fails.</returns>
+    public bool AddHotBarItem(UsableItem item, int index)
+    {
+        // TODO: Implement.
+        return false;
+    }
+
+    /// <summary>
+    /// Attempts to remove an item from the hotbar.
+    /// </summary>
+    /// <param name="index">The index of the item to be removed.</param>
+    /// <returns>The item being removed if it exists at that index, Null otherwise.</returns>
+    public UsableItem RemoveHotBarItem(int index)
+    {
+        // TODO: Implement.
+        return null;
+    }
+    #endregion
+
+    #region Utility Functions (quality of life for calculations)
+    /// <summary>
+    /// A quality of life function designed to sum all of the speed modifiers on equipped weapons and return the total percentage boost.
+    /// </summary>
+    /// <returns>Total percentage boost of speed modifiers on equipment.</returns>
+    public double GetSpeedModifier()
+    {
+        // TODO: Implement.
+        return 0.0;
+    }
+
+    /// <summary>
+    /// A quality of life function designed to sum all of the damage modifiers on equipped weapons and return the total percentage boost. 
+    /// </summary>
+    /// <returns>Total percentage boost of damage modifiers on equipment.</returns>
+    public double GetDamageModifier()
+    {
+        // TODO: Implement.
+        return 0.0;
+    }
+
+    /// <summary>
+    /// A quality of life function designed to sum all of the armor ratings of each piece of armor.
+    /// </summary>
+    /// <returns>Total armor rating of all armor pieces.</returns>
+    public double GetArmorRating()
+    {
+        // TODO: Implement.
+        return 0.0;
+    }
+    #endregion
+}
