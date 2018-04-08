00fa0f72cce807a7f1f8c559070a459b21194580
Craig Swearingen <cswear@illinoisrocstar.com>
2018-04-07 22:52:05 -0500

    Implemented button behavior of title screen

---
+++ Lumiere/Assets/Scripts/UI/UINavigationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UINavigationBehavior : MonoBehaviour 
+{
+	public GameObject nextScreen;
+	public Button button;
+	public GameObject currentScreen;
+
+	// Use this for initialization
+	void Start () 
+	{
+		button.onClick.AddListener(OnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnClick()
+	{
+		currentScreen.SetActive(false);
+		nextScreen.SetActive(true);
+	}
+}
