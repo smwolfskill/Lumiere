c76a8c8d8eee65b7ac9fefb32a098db527c3713e
rizaneeves <rizaneeves>
2018-04-14 17:52:17 -0500

    Add movement animation script

---
+++ Lumiere/Assets/Animations/MovementAnimation.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnimation : MonoBehaviour 
+{
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Dictionary<Direction, string> animationMapping;
+    private Direction lastDirection;
+
+    public enum Direction
+    {
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
+
+	// Use this for initialization
+	void Awake () 
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
+        lastDirection = Direction.NORTH;
+        animationMapping = new Dictionary<Direction, string> ();
+        animationMapping [Direction.NORTH] = "TNorth";
+        animationMapping [Direction.EAST] = "TEast";
+        animationMapping [Direction.WEST] = "TWest";
+        animationMapping [Direction.SOUTH] = "TSouth";
+	}
+	
+	void FixedUpdate () 
+    {
+        UpdateLastDirection ();
+        UpdateAnimation ();
+	}
+
+    void UpdateLastDirection()
+    {
+        velocity = rb.velocity;
+        if (velocity.x > 0) 
+        {
+            lastDirection = Direction.EAST;
+        } 
+        else if (velocity.x < 0) 
+        {
+            lastDirection = Direction.WEST;
+        } 
+        else if (velocity.y > 0) 
+        {
+            lastDirection = Direction.NORTH;
+        } 
+        else if (velocity.y < 0) 
+        {
+            lastDirection = Direction.SOUTH;
+        }
+    }
+
+    void UpdateAnimation()
+    {
+        float speed = velocity.magnitude;
+        string animationModifier = "";
+        if (speed < 0.1f) 
+        {
+            animationModifier = "Walk";
+        } 
+        else 
+        {
+            animationModifier = "Idle";    
+        }
+
+        anim.SetTrigger (animationMapping [lastDirection] + animationModifier);
+
+    }
+}
