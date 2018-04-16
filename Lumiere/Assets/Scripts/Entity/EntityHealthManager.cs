b9ac80eca3bbba7f18608898750a9f0e092324f9
Sheel Parekh <sdparek2@illinois.edu>
2018-04-16 14:07:23 -0400

    Add particle system to represent blood and damage and fix layers

--- Lumiere/Assets/Scripts/Entity/EntityHealthManager.cs
+++ Lumiere/Assets/Scripts/Entity/EntityHealthManager.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class EntityHealthManager : MonoBehaviour 
 {
     public EntityObject entityObj;
+    private ParticleSystem damagedParticles;
+
+    void Start()
+    {
+        damagedParticles = GetComponentInChildren<ParticleSystem>();
+    }
 
     public void InflictDamage(float damageAmount)
     {
         if (entityObj != null) 
         {
             entityObj.InflictDamage (damageAmount);
+            if(damagedParticles != null)
+            {
+                damagedParticles.Play();
+            }
         }
     }
 }
--- Lumiere/Assets/Scripts/Entity/Monster.cs
+++ Lumiere/Assets/Scripts/Entity/Monster.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "Lumiere/Entity/NPC/Monster")]
 public class Monster : NPC
 {
+    public GameObject damageParticles;
+
     override public GameObject Spawn(Map map, Vector2 location)
     {
         GameObject monster = base.Spawn(map, location);
+        GameObject particles = Instantiate(damageParticles, monster.transform);
         monster.layer = LayerMask.NameToLayer("Enemy");
         MonsterObject obj = new MonsterObject(monster, maxHealth);
+        
         monster.GetComponent<EntityHealthManager>().entityObj = obj;
         return monster;
     }
