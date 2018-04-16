0a78675052fe6736a4ed3a2ee4da19aea17429fb
Sheel Parekh <sdparek2@illinois.edu>
2018-04-16 19:01:25 -0400

    Add damage pop UI animation

--- Lumiere/Assets/Scripts/Entity/EntityHealthManager.cs
+++ Lumiere/Assets/Scripts/Entity/EntityHealthManager.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EntityHealthManager : MonoBehaviour 
 {
     public EntityObject entityObj;
     private ParticleSystem damagedParticles;
+    private GameObject damageUI;
 
     void Start()
     {
         damagedParticles = GetComponentInChildren<ParticleSystem>();
+        damageUI = GameObject.FindGameObjectWithTag("UIDamage");
     }
 
     public void InflictDamage(float damageAmount)
     {
         if (entityObj != null) 
         {
-            entityObj.InflictDamage (damageAmount);
-            if(damagedParticles != null)
+            if (damagedParticles != null)
             {
                 damagedParticles.Play();
             }
+            if (damageUI != null)
+            {
+                Text damageText = damageUI.GetComponent<Text>();
+                Animator damageAnimator = damageUI.GetComponent<Animator>();
+                damageText.text = ((int)damageAmount).ToString();
+                RectTransform damageTransform = damageUI.GetComponent<RectTransform>();
+                damageTransform.position = Camera.main.WorldToScreenPoint(transform.position);
+                damageAnimator.SetTrigger("Pop");
+            }
+            entityObj.InflictDamage (damageAmount);
+
         }
     }
 }
