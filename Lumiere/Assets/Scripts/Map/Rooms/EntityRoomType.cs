3064f71d3de2a37175bfbf8fd77d8aaf26bad820
WyattHRichter <WyattHRichter@gmail.com>
2018-04-21 20:15:54 -0500

    fixed tests that had to do with entity spawning

--- Lumiere/Assets/Scripts/Map/Rooms/EntityRoomType.cs
+++ Lumiere/Assets/Scripts/Map/Rooms/EntityRoomType.cs
@@ -15,35 +15,48 @@ public class EntityRoomType : RoomType
     /// <summary>
     /// Spawns the entities.
     /// </summary>
-    protected void SpawnEntities(Room room, Map map)
+    protected int SpawnEntities(Room room, Map map)
     {
-        List<Tile> walkableTiles = room.GetWalkableTiles ();
-        List<Vector2Int> spawnLocations = new List<Vector2Int> ();
 
         int minimunEntities = GetMinEntities(map);
         int maximumEntities = GetMaxEntities(map);
 
         int numEntities = Utilities.RandomIntInRange (minimunEntities, maximumEntities);
+
+        return SpawnEntities(numEntities, room, map);
+    }
+
+    public int SpawnEntities(int numEntities, Room room, Map map)
+    {
+        List<Tile> walkableTiles = room.GetWalkableTiles();
+        List<Vector2Int> spawnLocations = new List<Vector2Int>();
+
         entitiesSpawned = 0;
 
-        while (entitiesSpawned < numEntities) 
+        int numAttempts = 100;
+        int currAttempt = 0;
+
+        while (entitiesSpawned < numEntities && currAttempt < numAttempts)
         {
             //Debug.Log ("Number of tiles: " + tileObjs.Count);
             //Debug.Log ("Walkable Tiles: " + walkableTiles.Count);
-            Tile walkableTile = walkableTiles[Utilities.RandomIntInRange (0, walkableTiles.Count)];
-            Entity entityToSpawn = entities[Utilities.RandomIntInRange (0, entities.Length)];
-            Vector2Int tileLocation = new Vector2Int (walkableTile.x, walkableTile.y);
+            Tile walkableTile = walkableTiles[Utilities.RandomIntInRange(0, walkableTiles.Count)];
+            Entity entityToSpawn = entities[Utilities.RandomIntInRange(0, entities.Length)];
+            Vector2Int tileLocation = new Vector2Int(walkableTile.x, walkableTile.y);
 
-            if (!spawnLocations.Contains (tileLocation)) 
+            if (!spawnLocations.Contains(tileLocation))
             {
                 // Multiply location by tile offset to account for tile spacing or tile sizes
-                Vector2 locationToSpawn = new Vector2 (tileLocation.x * map.tileOffset, tileLocation.y * map.tileOffset);
-                entityToSpawn.Spawn (map, locationToSpawn);
-                spawnLocations.Add (tileLocation);
+                Vector2 locationToSpawn = new Vector2(tileLocation.x * map.tileOffset, tileLocation.y * map.tileOffset);
+                entityToSpawn.Spawn(map, locationToSpawn);
+                spawnLocations.Add(tileLocation);
                 entitiesSpawned++;
             }
+
+            currAttempt++;
         }
 
+        return entitiesSpawned;
     }
 
     public int GetMinEntities(Map map)
